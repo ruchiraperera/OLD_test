62726f6cb87fb65af083426ecf5140bda60d56cc
Ruchira Perera <ruchira.perera@s60.com.au>
2018-08-17 14:35:56 +0530

    updated

---
+++ json_xml_out_set_from_global/Controllers/testController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace json_xml_out_set_from_global.Controllers
+{
+    public class testController : ApiController
+    {
+        // GET: api/test
+
+        // global.asax
+
+        //GlobalConfiguration.Configuration.Formatters.JsonFormatter.MediaTypeMappings.Add(new QueryStringMapping("type", "json", new MediaTypeHeaderValue("application/json")));
+        //GlobalConfiguration.Configuration.Formatters.XmlFormatter.MediaTypeMappings.Add(new QueryStringMapping("type", "xml", new MediaTypeHeaderValue("application/xml")));
+
+        //api/jira?type=xml
+        //api/jira?type=json
+
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/test/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/test
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/test/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/test/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
